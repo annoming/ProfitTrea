cfd5135d1cd7c0d33969f04bfab0fdbafd478622
annoming <annoming_2899@163.com>
2021-05-11 16:56:58 +0800

    添加五档行情数据类

---
+++ src/main/java/com/atming/entity/FiveOrder.java
@@ -0,0 +1,276 @@
+package com.atming.entity;
+
+/**
+ * @author annoming
+ * @date 2021/4/29 4:21 下午
+ */
+
+public class FiveOrder {
+
+    /**证券代码*/
+    private String stockCode;
+
+    /**证券名称*/
+    private String stockName;
+
+    /**卖一*/
+    private String oneSale;
+
+    /**卖二*/
+    private String twoSale;
+
+    /**卖三*/
+    private String threeSale;
+
+    /**卖四*/
+    private String fourSale;
+
+    /**卖五*/
+    private String fiveSale;
+
+    /**买一*/
+    private String oneBuy;
+
+    /**买二*/
+    private String twoBuy;
+
+    /**买三*/
+    private String threeBuy;
+
+    /**买四*/
+    private String fourBuy;
+
+    /**买五*/
+    private String fiveBuy;
+
+    /**最新*/
+    private String nowNew;
+
+    /**涨幅*/
+    private String rate;
+
+    /**涨停*/
+    private String upStop;
+
+    /**跌停*/
+    private String downStop;
+
+    private String buyOne;
+    private String buyTwo;
+    private String buyThree;
+    private String buyFour;
+    private String buyFive;
+    private String saleOne;
+    private String saleTwo;
+    private String saleThree;
+    private String saleFour;
+    private String saleFive;
+
+    public String getStockCode() {
+        return stockCode;
+    }
+
+    public void setStockCode(String stockCode) {
+        this.stockCode = stockCode;
+    }
+
+    public String getStockName() {
+        return stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    public String getOneSale() {
+        return oneSale;
+    }
+
+    public void setOneSale(String oneSale) {
+        this.oneSale = oneSale;
+    }
+
+    public String getTwoSale() {
+        return twoSale;
+    }
+
+    public void setTwoSale(String twoSale) {
+        this.twoSale = twoSale;
+    }
+
+    public String getThreeSale() {
+        return threeSale;
+    }
+
+    public void setThreeSale(String threeSale) {
+        this.threeSale = threeSale;
+    }
+
+    public String getFourSale() {
+        return fourSale;
+    }
+
+    public void setFourSale(String fourSale) {
+        this.fourSale = fourSale;
+    }
+
+    public String getFiveSale() {
+        return fiveSale;
+    }
+
+    public void setFiveSale(String fiveSale) {
+        this.fiveSale = fiveSale;
+    }
+
+    public String getOneBuy() {
+        return oneBuy;
+    }
+
+    public void setOneBuy(String oneBuy) {
+        this.oneBuy = oneBuy;
+    }
+
+    public String getTwoBuy() {
+        return twoBuy;
+    }
+
+    public void setTwoBuy(String twoBuy) {
+        this.twoBuy = twoBuy;
+    }
+
+    public String getThreeBuy() {
+        return threeBuy;
+    }
+
+    public void setThreeBuy(String threeBuy) {
+        this.threeBuy = threeBuy;
+    }
+
+    public String getFourBuy() {
+        return fourBuy;
+    }
+
+    public void setFourBuy(String fourBuy) {
+        this.fourBuy = fourBuy;
+    }
+
+    public String getFiveBuy() {
+        return fiveBuy;
+    }
+
+    public void setFiveBuy(String fiveBuy) {
+        this.fiveBuy = fiveBuy;
+    }
+
+    public String getNowNew() {
+        return nowNew;
+    }
+
+    public void setNowNew(String nowNew) {
+        this.nowNew = nowNew;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    public String getUpStop() {
+        return upStop;
+    }
+
+    public void setUpStop(String upStop) {
+        this.upStop = upStop;
+    }
+
+    public String getDownStop() {
+        return downStop;
+    }
+
+    public void setDownStop(String downStop) {
+        this.downStop = downStop;
+    }
+
+    public String getBuyOne() {
+        return buyOne;
+    }
+
+    public void setBuyOne(String buyOne) {
+        this.buyOne = buyOne;
+    }
+
+    public String getBuyTwo() {
+        return buyTwo;
+    }
+
+    public void setBuyTwo(String buyTwo) {
+        this.buyTwo = buyTwo;
+    }
+
+    public String getBuyThree() {
+        return buyThree;
+    }
+
+    public void setBuyThree(String buyThree) {
+        this.buyThree = buyThree;
+    }
+
+    public String getBuyFour() {
+        return buyFour;
+    }
+
+    public void setBuyFour(String buyFour) {
+        this.buyFour = buyFour;
+    }
+
+    public String getBuyFive() {
+        return buyFive;
+    }
+
+    public void setBuyFive(String buyFive) {
+        this.buyFive = buyFive;
+    }
+
+    public String getSaleOne() {
+        return saleOne;
+    }
+
+    public void setSaleOne(String saleOne) {
+        this.saleOne = saleOne;
+    }
+
+    public String getSaleTwo() {
+        return saleTwo;
+    }
+
+    public void setSaleTwo(String saleTwo) {
+        this.saleTwo = saleTwo;
+    }
+
+    public String getSaleThree() {
+        return saleThree;
+    }
+
+    public void setSaleThree(String saleThree) {
+        this.saleThree = saleThree;
+    }
+
+    public String getSaleFour() {
+        return saleFour;
+    }
+
+    public void setSaleFour(String saleFour) {
+        this.saleFour = saleFour;
+    }
+
+    public String getSaleFive() {
+        return saleFive;
+    }
+
+    public void setSaleFive(String saleFive) {
+        this.saleFive = saleFive;
+    }
+}
